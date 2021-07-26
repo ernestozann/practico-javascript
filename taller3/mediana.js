0df717ebfad11dee492f3fa6f27f04164b2cdce1
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-07-26 09:38:29 -0600

    calculando la mediana en JS '3'

---
+++ taller3/mediana.js
@@ -0,0 +1,55 @@
+function calcularMediaAritmetica(lista) {
+	const sumaLista = lista.reduce(
+		function (valorAcumulado = 0, nuevoElemento) {
+			return valorAcumulado + nuevoElemento;
+		}
+	);
+	const promedioLista = sumaLista / lista.length;
+	return promedioLista;
+}
+
+const lista1 = [
+	100,
+	200,
+	500,
+	400000000,
+];
+
+//reto
+const numeros = [
+	3,
+	23,
+	12,
+	42,
+	100,
+	1000,
+];
+
+numeros.sort(function(a, b) {
+	return a - b;
+});
+//reto completado
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function esPar(numero) {
+	if (numero % 2 === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+let mediana;
+
+if (esPar(lista1.length)) {
+	const elemento1 = lista1[mitadLista1 - 1];
+	const elemento2 = lista1[mitadLista1];
+
+	const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+	mediana = promedioElemento1y2;
+	console.log(`La mediana de tu lista es: ${mediana}`);
+} else {
+	mediana = mitadLista1;
+	console.log("La mediana de tu lista es: " + lista1[mediana]);
+}
--- taller3/promedio.js
+++ taller3/promedio.js
@@ -9,12 +9,14 @@
 // console.log("el promedio es: " + promedioLista1);
 
 function calcularMediaAritmetica(lista) {
+	//Con ciclo for
 	// let sumaLista = 0;
 
 	// for (let i = 0; i < lista.length; i++) {
 	// 	sumaLista = sumaLista + lista[i];
 	// }
 
+	//con reduce
 	const sumaLista = lista.reduce(
 		function (valorAcumulado = 0, nuevoElemento) {
 			return valorAcumulado + nuevoElemento;
